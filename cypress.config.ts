17be191624b7cba3d76b985d634db7887dfd9575
Łukasz Mikołajczyk <lmikolajczyk@egnyte.com>
2024-03-17 22:07:19 +0100

    add screenshots & videos

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,30 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress"
+import * as fs from "fs"
 
 export default defineConfig({
-  viewportWidth: 1201,
-  viewportHeight: 800,
-  chromeWebSecurity: false,
-  e2e: {
-    baseUrl: 'http://www.nobl9.com',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    viewportWidth: 1201,
+    viewportHeight: 800,
+    chromeWebSecurity: false,
+    watchForFileChanges: false,
+    screenshotOnRunFailure: true,
+    video: true,
+    videoCompression: true,
+    e2e: {
+        baseUrl: "http://www.nobl9.com",
+        setupNodeEvents(on) {
+            on(
+                "after:spec",
+                (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+                    if (results && results.video) {
+                        const failures = results.tests.some((test) =>
+                            test.attempts.some((attempt) => attempt.state === "failed")
+                        )
+                        if (!failures) {
+                            fs.unlinkSync(results.video)
+                        }
+                    }
+                }
+            )
+        },
     },
-  },
-});
+})
